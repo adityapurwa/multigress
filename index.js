afeac9d3b678ae3fb12e4d485190bc460c04ff30
Aditya Purwa <adityapurwa@windowslive.com>
2018-06-08 13:21:46 +0700

    move to port 8000

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const app = express();
 app.use(express.static('public'));
 app.use('/dist', express.static('dist'));
 
-app.listen(80);
+app.listen(8000);
 
-console.log('Running Multigress at *:80');
+console.log('Running Multigress at *:8000');
